dae141a9c1f311b6543220b2896f1243b80fea49
Mohamed-Omar <mero2online@gmail.com>
2020-10-10 11:08:10 +0200

    Add Scroll to anchor ID using scrollTO event

--- js/app.js
+++ js/app.js
@@ -11,21 +11,22 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
 const navbarList = document.getElementById('navbar__list')
 const allSections = document.querySelectorAll('section')
+const allMenuLinks = document.querySelectorAll('.menu__link')
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -33,25 +34,34 @@ const allSections = document.querySelectorAll('section')
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // Build the nav & menu and scroll to section on link click
 
 allSections.forEach(item => {
     let navListItems = item = `<li><a href="#${item.id}" class="menu__link" data-menu-link="${item.id}">${item.getAttribute("data-nav")}</a></li>`
     navbarList.insertAdjacentHTML('beforeend', navListItems);
-})
-
-// Set sections as active
+});
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+document.querySelectorAll('.menu__link').forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        let ele = document.getElementById(`${link.getAttribute("data-menu-link")}`)
+        ele.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+});
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
+
+// Set sections as active in navbar
